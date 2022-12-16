77a3c382c7eb08a467297cc236314ea146362b21
Ali Ahmadov <aahmadov7626@ada.edu.az>
2022-12-16 18:15:07 +0400

    all maps done

--- TurboAz/Domain/Entities/Mapping/BanTypeMap.cs
+++ TurboAz/Domain/Entities/Mapping/BanTypeMap.cs
@@ -1,12 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TurboAz.Domain.Entities.Mapping
 {
-    internal class BanTypeMap
+    public class BanTypeMap:EntityTypeConfiguration<BanType>
     {
+        public BanTypeMap()
+        {
+            this.ToTable("BanTypes");
+
+            this.HasKey(c => c.Id);
+
+            this.Property(c => c.TypeName)
+                .HasMaxLength(30)
+                .IsRequired();
+            this.Property(c => c.TypeName).HasColumnName("TypeName");
+
+            this.HasMany(c => c.Cars)
+                .WithRequired()
+                .HasForeignKey(c => c.BanTypeId)
+                .WillCascadeOnDelete(true);
+
+
+        }
     }
 }
--- TurboAz/Domain/Entities/Mapping/ModelMap.cs
+++ TurboAz/Domain/Entities/Mapping/ModelMap.cs
@@ -7,10 +7,24 @@ using System.Threading.Tasks;
 
 namespace TurboAz.Domain.Entities.Mapping
 {
-    public class ModelMap:EntityTypeConfiguration
+    public class ModelMap:EntityTypeConfiguration<Model>
     {
 
+        public ModelMap()
+        {
+            this.ToTable("Models");
 
+            this.HasKey(m => m.Id);
+
+            this.Property(m => m.Name)
+                .IsRequired()
+                .HasMaxLength(30);
+            this.Property(m => m.Name).HasColumnName("ModelName");
+
+
+            HasRequired(c => c.Car).WithRequiredPrincipal();
+
+        }
 
     }
 }
